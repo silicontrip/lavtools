384b5175f8ae6c9fddd646b20356a01dccaa16d6
Mark Heath <mark@silicontrip.net>
2010-09-09 00:02:26 +1000

    yuvtbilateral first draft working. TODO: Clean some pipeline bugs.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 LDFLAGS=-L/usr/local/lib -lmjpegutils -L/opt/local/lib
-#CFLAGS=-O3 -I/usr/local/include/mjpegtools -I/opt/local/include -I/usr/local/include
-CFLAGS=-g -I/usr/local/include/mjpegtools -I/opt/local/include -I/usr/local/include
+CFLAGS=-O3 -I/usr/local/include/mjpegtools -I/opt/local/include -I/usr/local/include
+#CFLAGS=-g -I/usr/local/include/mjpegtools -I/opt/local/include -I/usr/local/include
 
 all: libav-bitrate libav2yuv libavmux yuvaddetect yuvadjust yuvaifps yuvconvolve yuvcrop yuvdeinterlace yuvdiag yuvdiff yuvfade yuvhsync yuvrfps yuvtshot yuvwater
 
--- yuvtbilateral.c
+++ yuvtbilateral.c
@@ -20,7 +20,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
-gcc yuvdeinterlace.c -I/sw/include/mjpegtools -lmjpegutils  
+ gcc yuvdeinterlace.c -I/sw/include/mjpegtools -lmjpegutils  
  */
 
 #ifdef HAVE_CONFIG_H
@@ -33,15 +33,15 @@ gcc yuvdeinterlace.c -I/sw/include/mjpegtools -lmjpegutils
 #include <unistd.h>
 #include <signal.h>
 #include <string.h>
-
+#include <math.h>
 
 #include "yuv4mpeg.h"
 #include "mpegconsts.h"
-#include "utilyuv.c"
+#include "utilyuv.h"
 
 #define VERSION "0.1"
 
-#define PRECISION 256
+#define PRECISION 512
 
 struct parameters {
 	
@@ -49,6 +49,8 @@ struct parameters {
 	unsigned int sigmaR;
 	
 	unsigned int kernelRadius;
+	unsigned int kernelSize;
+	
 	
 	unsigned int *kernelD;
 	unsigned int *kernelR;
@@ -68,27 +70,29 @@ static void print_usage()
 {
 	fprintf (stderr,
 			 "usage: yuv\n"
-			);
+			 );
 }
 
 
 unsigned int gauss (unsigned int sigma, int x, int y) {
-	return exp(-((x * x + y * y) / (2.0 * (1.0* sigma/PRECISION) * (1.0*sigma/PRECISION)))) * PRECISION;
+	
+	// fprintf (stderr,"%f , %d %d\n", (1.0* sigma/PRECISION) ,x,y);
+	
+	//float sigmaf =  (1.0* sigma/PRECISION)
+	
+	return (exp(-((1.0 * x * x) / (2.0 * (1.0* sigma/PRECISION) * (1.0*sigma/PRECISION))))) * PRECISION;
 }
 
 unsigned int similarity(int p, int s) {
 	// this equals: Math.exp(-(( Math.abs(p-s)) /  2 * this.sigmaR * this.sigmaR));
 	// but is precomputed to improve performance
-	if (this.direction == 0) 
 		return this.gaussSimilarity[abs(p-s)];
-	
-	return this.gaussSimilarity[255-abs(p-s)];
-	
+		
 }
 
 static void filterinitialize () {
 	
-	int kernelSize, center;
+	int center;
 	int x,y,i;
 	
 	this.kernelRadius = this.sigmaD>this.sigmaR?this.sigmaD * 2:this.sigmaR * 2;
@@ -97,7 +101,7 @@ static void filterinitialize () {
 	this.twoSigmaRSquared = (2 * (1.0 *this.sigmaR/PRECISION)  * (1.0 *this.sigmaR/PRECISION)) * PRECISION;
 	
 	this.kernelSize = this.kernelRadius * 2 + 1;
-	center = (kernelSize - 1) / 2;
+	center = (this.kernelSize - 1) / 2;
 	
 	
 	this.kernelD = (unsigned int*) malloc( sizeof (unsigned int) * this.kernelSize );
@@ -106,12 +110,20 @@ static void filterinitialize () {
 		mjpeg_error_exit1("Cannot allocate memory for filter kernel");
 	}
 	
+	fprintf(stderr,"size %d, radius %d center %d\n",this.kernelSize,this.kernelRadius,center);
+
+	x = -center;
+	//fprintf (stderr,"x %d < %d\n",x,this.kernelSize - center);
+
 	
-	for ( x = -center; x < -center + this.kernelSize; x++) {
-			this.kernelD[x + center] = gauss(this.sigmaD, x, 0);
-			//fprintf(stderr,"x: %d y: %d = %d\n",x,y,this.kernelD[x + center + (y + center) * kernelSize]);
+	for ( x=0; x < this.kernelSize ; x++) {
+	//	fprintf (stderr,"x %d\n",x);
+		this.kernelD[x ] = gauss(this.sigmaD, x-center, 0);
+	//	fprintf(stderr,"x: %d  = %d\n",x,this.kernelD[x]);
 	}
 	
+	// fprintf (stderr,"gauss Similarity malloc\n",i);
+
 	this.gaussSimilarity = (unsigned int*) malloc(sizeof (unsigned int) * 256);
 	if (this.gaussSimilarity == NULL ){
 		free(this.kernelD);
@@ -121,6 +133,7 @@ static void filterinitialize () {
 	// precomute all possible similarity values for
 	// performance reasons
 	for ( i = 0; i < 256; i++) {
+		//fprintf (stderr,"i %d\n",i);
 		this.gaussSimilarity[i] = exp(-((i) / (1.0 * this.twoSigmaRSquared/PRECISION))) * PRECISION;
 	}
 	
@@ -129,13 +142,35 @@ static void filterinitialize () {
 
 
 
-static void filterpixel(uint8_t *o, uint8_t *p, int i, int j, int w, int h) {
-
-	o[i+j*w] = p[i+j*w];
+static void filterpixel(uint8_t **o, uint8_t ***p,int chan, int i, int j, int w, int h) {
+	
+	unsigned int sum =0;
+	unsigned int totalWeight = 0;
+	int weight;
+	int z;
+	
+	uint8_t intensityCenter = p[this.kernelSize/2-1][chan][j * w + i];
+	
+	for ( z = 0; z < this.kernelSize; z++) {
+		
+		int intensityKernelPos = p[z][chan][i + j * w];
+		
+		//fprintf (stderr,"kernel D %d. kp %d cen %d\n",this.kernelD[z],intensityKernelPos,intensityCenter);
+		
+		weight = this.kernelD[z] * similarity(intensityKernelPos,intensityCenter);
+		totalWeight += weight;
+		sum += (weight * intensityKernelPos);
+		
+	}
+//	fprintf(stderr,"total %d\n",totalWeight);
+	if (totalWeight > 0 )
+		o[chan][i+j*w] = sum / totalWeight;
+	else
+		o[chan][i+j*w] = intensityCenter;
 	
 }
 
-static void filterframe (uint8_t *m[3], uint8_t *n[][], y4m_stream_info_t *si)
+static void filterframe (uint8_t *m[3], uint8_t ***n, y4m_stream_info_t *si)
 {
 	
 	int x,y;
@@ -151,24 +186,12 @@ static void filterframe (uint8_t *m[3], uint8_t *n[][], y4m_stream_info_t *si)
 	
 	for (y=0; y < height; y++) {
 		for (x=0; x < width; x++) {
-			
-			// filterpixel(m[0],n[0],x,y,width,height);
-			
-			for ( n = 0; n < this.kernelSize; n++) {
-				
-					int intensityKernelPos = n[y + n * w];
 					
-					weight = this.kernelD[(i-m + this.kernelRadius) + (j-n + this.kernelRadius) * (this.kernelRadius*2)] * similarity(intensityKernelPos,intensityCenter);
-					totalWeight += weight;
-					sum += (weight * intensityKernelPos);
-			
-			}
-			
-			
+			filterpixel(m,n,0,x,y,width,height);
 			
 			if (x<width2 && y<height2) {
-				filterpixel(m[1],n[1],x,y,width2,height2);
-				filterpixel(m[2],n[2],x,y,width2,height2);
+				filterpixel(m,n,1,x,y,width2,height2);
+				filterpixel(m,n,2,x,y,width2,height2);
 			}
 			
 		}
@@ -177,29 +200,30 @@ static void filterframe (uint8_t *m[3], uint8_t *n[][], y4m_stream_info_t *si)
 }
 
 
-static void filter(  int fdIn  , y4m_stream_info_t  *inStrInfo )
+static void filter(  int fdIn ,int fdOut , y4m_stream_info_t  *inStrInfo )
 {
 	y4m_frame_info_t   in_frame ;
-	uint8_t            *yuv_data[][];
+	uint8_t            ***yuv_data;
 	uint8_t				*yuv_odata[3];
 	int                read_error_code ;
 	int                write_error_code ;
-	int c,kernelSize;
+	int c;
 	
 	// Allocate memory for the YUV channels
 	
-
+	
 	
 	if (chromalloc(yuv_odata,inStrInfo))		
 		mjpeg_error_exit1 ("Could'nt allocate memory for the YUV4MPEG data!");
 	
 	// sanity check!!!
-	yuv_data= (uint8_t *) malloc(sizeof (uint8_t *) * this.kernelSize);
+	yuv_data= (uint8_t ***) malloc(sizeof (uint8_t *) * this.kernelSize);
 	if (yuv_data == NULL) {
 		chromafree(yuv_odata);
 		mjpeg_error_exit1("cannot allocate memory for frame buffer");
+	}
 	for (c=0;c<this.kernelSize;c++) {
-		yuv_data[c] = (uint8_t *) malloc(sizeof (uint8_t *) * 3);
+		yuv_data[c] = (uint8_t **) malloc(sizeof (uint8_t *) * 3);
 		if (yuv_data[c] == NULL) {
 			// how am I going to keep track of what I've allocated?
 			mjpeg_error_exit1("cannot allocate memory for frame buffer");
@@ -208,28 +232,28 @@ static void filter(  int fdIn  , y4m_stream_info_t  *inStrInfo )
 			mjpeg_error_exit1("cannot allocate memory for frame buffer");
 		}
 	}
-		
+	
 	/* Initialize counters */
 	
 	write_error_code = Y4M_OK ;
 	
 	y4m_init_frame_info( &in_frame );
-	read_error_code = y4m_read_frame(fdIn, inStrInfo,&in_frame,yuv_data[kernelSize-1] );
+	read_error_code = y4m_read_frame(fdIn, inStrInfo,&in_frame,yuv_data[this.kernelSize-1] );
 	
 	//init loop mode
-		for (c=this.kernelSize-1;c>0;c--) {
-			chromacpy(yuv_data[c-1],yuv_data[c]);
+	for (c=this.kernelSize-1;c>0;c--) {
+		chromacpy(yuv_data[c-1],yuv_data[c],inStrInfo);
+	}
+	
+	for(c=0;c<this.kernelRadius;c++) {
+		read_error_code = y4m_read_frame(fdIn, inStrInfo,&in_frame,yuv_data[this.kernelSize-1] );
+		// shuffle
+		// should use pointers...
+		for (c=0;c<this.kernelSize-1;c++) {
+			chromacpy(yuv_data[c],yuv_data[c+1],inStrInfo);
 		}
 		
-		for(c=0;c<this.kernelRadius;c++) {
-			read_error_code = y4m_read_frame(fdIn, inStrInfo,&in_frame,yuv_data[kernelSize-1] );
-			// shuffle
-			// should use pointers...
-			for (c=0;c<kernelSize-1;c++) {
-				chromacpy(yuv_data[c],yuv_data[c+1]);
-			}
-			
-		}
+	}
 	
 	while( Y4M_ERR_EOF != read_error_code && write_error_code == Y4M_OK ) {
 		
@@ -241,39 +265,39 @@ static void filter(  int fdIn  , y4m_stream_info_t  *inStrInfo )
 		
 		y4m_fini_frame_info( &in_frame );
 		y4m_init_frame_info( &in_frame );
-		read_error_code = y4m_read_frame(fdIn, inStrInfo,&in_frame,yuv_data[kernelSize-1] );
+		read_error_code = y4m_read_frame(fdIn, inStrInfo,&in_frame,yuv_data[this.kernelSize-1] );
 		// shuffle
 		// should use pointers...
-		for (c=0;c<kernelSize-1;c++) {
-			chromacpy(yuv_data[c],yuv_data[c+1]);
+		for (c=0;c<this.kernelSize-1;c++) {
+			chromacpy(yuv_data[c],yuv_data[c+1],inStrInfo);
 		}
 		
 	}
-// finalise loop		
-		for(c=0;c<this.kernelRadius;c++) {
-			if (read_error_code == Y4M_OK) {
-				filterframe(yuv_odata,yuv_data,inStrInfo);
-				write_error_code = y4m_write_frame( fdOut, inStrInfo, &in_frame, yuv_odata );
-			}
-			
-			y4m_fini_frame_info( &in_frame );
-			y4m_init_frame_info( &in_frame );
-			
-			// shuffle
-			// should use pointers...
-			for (c=0;c<kernelSize-1;c++) {
-				chromacpy(yuv_data[c],yuv_data[c+1]);
-			}
-			
+	// finalise loop		
+	for(c=0;c<this.kernelRadius;c++) {
+		if (read_error_code == Y4M_OK) {
+			filterframe(yuv_odata,yuv_data,inStrInfo);
+			write_error_code = y4m_write_frame( fdOut, inStrInfo, &in_frame, yuv_odata );
 		}
 		
+		y4m_fini_frame_info( &in_frame );
+		y4m_init_frame_info( &in_frame );
 		
+		// shuffle
+		// should use pointers...
+		for (c=0;c<this.kernelSize-1;c++) {
+			chromacpy(yuv_data[c],yuv_data[c+1],inStrInfo);
+		}
+		
+	}
+	
+	
 	// Clean-up regardless an error happened or not
-		
-		
+	
+	
 	y4m_fini_frame_info( &in_frame );
 	chromafree(yuv_odata);
-		
+	
 	if( read_error_code != Y4M_ERR_EOF )
 		mjpeg_error_exit1 ("Error reading from input stream!");
 	
@@ -293,6 +317,7 @@ int main (int argc, char *argv[])
 	y4m_ratio_t frame_rate;
 	int interlaced,ilace=0,pro_chroma=0,yuv_interlacing= Y4M_UNKNOWN;
 	int height;
+	float sigma;
 	int c ;
 	const static char *legal_flags = "?hv:r:d:";
 	
@@ -304,8 +329,8 @@ int main (int argc, char *argv[])
 					mjpeg_error_exit1 ("Verbose level must be [0..2]");
 				break;
 				
-				case 'h':
-				case '?':
+			case 'h':
+			case '?':
 				print_usage (argv);
 				return 0 ;
 				break;
@@ -322,6 +347,13 @@ int main (int argc, char *argv[])
 		}
 	}
 	
+	if (this.sigmaR == 0 || this.sigmaD == 0) {
+		print_usage();
+		mjpeg_error_exit1("Sigma D and R must be set");
+		
+	}
+	
+	
 	// mjpeg tools global initialisations
 	mjpeg_default_handler_verbosity (verbose);
 	
@@ -337,14 +369,16 @@ int main (int argc, char *argv[])
 		mjpeg_error_exit1 ("Could'nt read YUV4MPEG header!");
 	
 	// Information output
-	mjpeg_info ("yuvtbilateral (version " YUVDE_VERSION ") is a general deinterlace/interlace utility for yuv streams");
+	mjpeg_info ("yuvtbilateral (version " VERSION ") is a general deinterlace/interlace utility for yuv streams");
 	mjpeg_info ("(C) 2010 Mark Heath <mjpeg0 at silicontrip.org>");
 	mjpeg_info (" -h for help");
 	
-    
+	
 	/* in that function we do all the important work */
 	filterinitialize ();
-	filter(fdIn, &in_streaminfo);
+	y4m_write_stream_header(fdOut,&in_streaminfo);
+
+	filter(fdIn, fdOut, &in_streaminfo);
 	
 	y4m_fini_stream_info (&in_streaminfo);
 	
