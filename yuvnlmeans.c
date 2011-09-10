97c0bdaf5d8e3443737283cebfaeeee38ed24506
Mark Heath <mark@silicontrip.net>
2011-09-10 20:43:47 +1000

    added B algorithm.  Huge speed improvement.  Changed doubles to fixed int precision.

--- yuvnlmeans.c
+++ yuvnlmeans.c
@@ -57,20 +57,15 @@ gcc yuvdeinterlace.c -I/sw/include/mjpegtools -lmjpegutils
 
 struct parameters {
 
-	unsigned int sigmaD;
-	unsigned int sigmaR;
-	
-	unsigned int kernelRadius;
-	
-	unsigned int *kernelD;
-	unsigned int *kernelR;
-	
-	unsigned int *gaussSimilarity;
-	
-	unsigned int twoSigmaRSquared;
-	
-	int direction;
-	
+	int Ax,Ay,Bx,By,Sx,Sy,a,h;
+	// want to change these into fixed precision
+	int *gwh;
+	int *dsum;
+	int *dweight;
+	int *dwmax;
+	int *gw;
+	int *sumsb;
+	int *weightsb;
 };
 
 static struct parameters this;
@@ -89,99 +84,237 @@ unsigned int gauss (unsigned int sigma, int x, int y) {
 	return exp(-((x * x + y * y) / (2.0 * (1.0* sigma/PRECISION) * (1.0*sigma/PRECISION)))) * PRECISION;
 }
 
-unsigned int similarity(int p, int s) {
-	// this equals: Math.exp(-(( Math.abs(p-s)) /  2 * this.sigmaR * this.sigmaR));
-	// but is precomputed to improve performance
-	if (this.direction == 0) 
-		return this.gaussSimilarity[abs(p-s)];
-	
-	return this.gaussSimilarity[255-abs(p-s)];
 
+
+static void filteruninitialize() {
+	
+	free(this.gwh);
+	free(this.gw);
+	free(this.dsum);
+	free(this.dweight);
+	free(this.dwmax);
 }
+static void filterinitialize (int Ax, int Ay, int Bx, int By, int Sx, int Sy, int a, int h, y4m_stream_info_t  *inStrInfo ) {
 
-static void filterinitialize () {
-
-	int kernelSize, center;
-	int x,y,i;
 	
-	this.kernelRadius = this.sigmaD>this.sigmaR?this.sigmaD * 2:this.sigmaR * 2;
-	this.kernelRadius = this.kernelRadius / PRECISION;
+	// Window size
+	// trial values, will move to Command line arguments
+	this.a = a;
+	this.h = h;
 	
-	this.twoSigmaRSquared = (2 * (1.0 *this.sigmaR/PRECISION)  * (1.0 *this.sigmaR/PRECISION)) * PRECISION;
+	this.Ax = Ax; 
+	this.Ay = Ay;
 	
-	kernelSize = this.kernelRadius * 2 + 1;
-	center = (kernelSize - 1) / 2;
+	this.Sy = Sy;
+	this.Sx = Sx;
 	
-	
-	this.kernelD = (unsigned int*) malloc( sizeof (unsigned int) * kernelSize * kernelSize);
-	
-	if (this.kernelD == NULL ){
-		mjpeg_error_exit1("Cannot allocate memory for filter kernel");
-	}
-		
-	
-	for ( x = -center; x < -center + kernelSize; x++) {
-		for ( y = -center; y < -center + kernelSize; y++) {
-			this.kernelD[x + center + (y + center) * kernelSize] = gauss(this.sigmaD, x, y);
-			//fprintf(stderr,"x: %d y: %d = %d\n",x,y,this.kernelD[x + center + (y + center) * kernelSize]);
-		}
-	}
-	
-	this.gaussSimilarity = (unsigned int*) malloc(sizeof (unsigned int) * 256);
-	if (this.gaussSimilarity == NULL ){
-		free(this.kernelD);
-		mjpeg_error_exit1("Cannot allocate memory for gaussian curve");
-	}
-	
-	// precomute all possible similarity values for
-	// performance reasons
-	for ( i = 0; i < 256; i++) {
-		this.gaussSimilarity[i] = exp(-((i) / (1.0 * this.twoSigmaRSquared/PRECISION))) * PRECISION;
-	}
-	
-	
-}
-
-static void filterpixel(uint8_t *o, uint8_t *p, int i, int j, int w, int h) {
-
-	unsigned int sum =0;
-	unsigned int totalWeight = 0;
-	int weight;
-	int m,n;
-	
-	uint8_t intensityCenter = p[j * w + i];
-	int mMax = i + this.kernelRadius;
-	int nMax = j + this.kernelRadius;
-	
-	for ( m = i-this.kernelRadius; m < mMax; m++) {
-		for ( n = j-this.kernelRadius; n < nMax; n++) {
-			
-			if (m>=0 && n>=0 && m < w && n < h) {
-				int intensityKernelPos = p[m + n * w];
-				
-				weight = this.kernelD[(i-m + this.kernelRadius) + (j-n + this.kernelRadius) * (this.kernelRadius*2)] * similarity(intensityKernelPos,intensityCenter);
-				totalWeight += weight;
-				sum += (weight * intensityKernelPos);
-			}
-		}
-	}
-	if (totalWeight > 0 )
-	o[j * w + i] = (sum / totalWeight);
-	else
-	o[j * w + i] = intensityCenter;
-}
-
-
-static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, double *dssum, double *dsweight, double *dswmax, int Ax, int Ay, int Sx, int Sy, int h, double *gw)
-{
-	
-	int b;
+	this.Bx = Bx;
+	this.By = By;
 	
 	int Sxd = Sx*2+1;
 	int Syd = Sy*2+1;
 	
-	double 	h2in = -1.0/(h*h);
-	double hin = -1.0/h;
+	int Sxa = Sxd*Syd;
+	int Bxd = Bx*2+1;
+	int Byd = By*2+1;
+	int Bxa = Bxd*Byd;
+	int Axd = Ax*2+1;
+	int Ayd = Ay*2+1;
+	int Axa = Axd*Ayd;
+	// Azdm1 = Az*2;
+	int a2 = a*a;
+	
+	
+	const int Sxh = (Sx+1)>>1;
+	const int Syh = (Sy+1)>>1;
+	const int Sxdh = Sxh*2+1;
+	const int Sydh = Syh*2+1;
+	const int Bxh = (Bx+1)>>1;
+	const int Byh = (By+1)>>1;
+	
+	
+	
+	this.gwh = (int*)malloc(Sxdh*Sydh*sizeof(int));
+	int w = 0, m, n;
+	int j;
+	for ( j=-Syh; j<=Syh; ++j)
+	{
+		if (j < 0) m = MIN(j+Byh,0);
+		else m = MAX(j-Byh,0);
+		int k;
+		for ( k=-Sxh; k<=Sxh; ++k)
+		{
+			if (k < 0) n = MIN(k+Bxh,0);
+			else n = MAX(k-Bxh,0);
+			this.gwh[w++] = exp(-((m*m+n*n)/(2*a2))) * PRECISION;
+		}
+	}
+	
+	int framesize = y4m_si_get_plane_length(inStrInfo,0);
+	
+	this.dsum = (int *)malloc(framesize*sizeof(int));
+	this.dweight = (int *)malloc(framesize*sizeof(int));
+	this.dwmax = (int *)malloc(framesize*sizeof(int));
+	
+	this.gw = (int*)malloc(Sxd*Syd*sizeof(int));
+	
+	//	int w = 0, m, n;
+	// int j;
+	for ( j=-Sy; j<=Sy; ++j)
+	{
+		if (j < 0) m = MIN(j+By,0);
+		else m = MAX(j-By,0);
+		int k;
+		for ( k=-Sx; k<=Sx; ++k)
+		{
+			if (k < 0) n = MIN(k+Bx,0);
+			else n = MAX(k-Bx,0);
+			this.gw[w++] = exp(-((m*m+n*n)/(2*a2))) * PRECISION;
+		}
+	}
+	
+	if (Bx || By)
+	{
+		this.sumsb = (int *)malloc(Bxa*sizeof(int));
+	//	if (!sumsb) env->ThrowError("TNLMeans:  malloc failure (sumsb)!");
+		this.weightsb = (int *)malloc(Bxa*sizeof(int));
+	//	if (!weightsb) env->ThrowError("TNLMeans:  malloc failure (weightsb)!");
+	}
+	
+	
+}
+
+static void filterframeB  (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si) {
+
+	int b;
+	
+	int 	Byd = this.By*2+1;
+	int 	Bxd = this.Bx*2+1;
+	int 	Bxa = Bxd*Byd;
+	int		Sxd = this.Sx*2+1;
+	double hin = -1.0/this.h;
+	
+	for (b=0; b<3; ++b)
+	{
+		
+		const unsigned char *srcp = n[b];
+		const unsigned char *pfp = n[b];
+		unsigned char *dstp = m[b];
+		const int height = y4m_si_get_plane_height(si,b);
+		const int heightm1 = height-1;
+		const int width = y4m_si_get_plane_width(si,b);
+		const int widthm1 = width-1;
+		const int pitch = width ;
+		int *sumsb_saved = this.sumsb+this.Bx;
+		int *weightsb_saved = this.weightsb+this.Bx;
+		int y;
+		for (y=this.By; y<height+this.By; y+=Byd)
+		{
+			const int starty = MAX(y-this.Ay,this.By);
+			const int stopy = MIN(y+this.Ay,heightm1-MIN(this.By,heightm1-y));
+			const int yTr = MIN(Byd,height-y+this.By);
+			
+			int x;
+			for ( x=this.Bx; x<width+this.Bx; x+=Bxd)
+			{
+				memset(this.sumsb,0,Bxa*sizeof(int));
+				memset(this.weightsb,0,Bxa*sizeof(int));
+				int wmax = 0;
+				const int startx = MAX(x-this.Ax,this.Bx);
+				const int stopx = MIN(x+this.Ax,widthm1-MIN(this.Bx,widthm1-x));
+				const int xTr = MIN(Bxd,width-x+this.Bx);
+				int u;
+				for ( u=starty; u<=stopy; ++u)
+				{
+					const int yT = -MIN(MIN(this.Sy,u),y);
+					const int yB = MIN(MIN(this.Sy,heightm1-u),heightm1-y);
+					const int yBb = MIN(MIN(this.By,heightm1-u),heightm1-y);
+					const unsigned char *s1_saved = pfp + (u+yT)*pitch;
+					const unsigned char *s2_saved = pfp + (y+yT)*pitch + x;
+					const unsigned char *sbp_saved = pfp + (u-this.By)*pitch;
+					const int *gw_saved = this.gw+(yT+this.Sy)*Sxd+this.Sx;
+					int v;
+					for ( v=startx; v<=stopx; ++v)
+					{
+						if (u == y && v == x) continue;
+						const int xL = -MIN(MIN(this.Sx,v),x);
+						const int xR = MIN(MIN(this.Sx,widthm1-v),widthm1-x);
+						const unsigned char *s1 = s1_saved + v;
+						const unsigned char *s2 = s2_saved;
+						const int *gwT = gw_saved;
+						int diff = 0, gweights = 0;
+						int j;
+						for ( j=yT; j<=yB; ++j)
+						{
+							int k;
+							for ( k=xL; k<=xR; ++k)
+							{
+								diff += ABS(s1[k]-s2[k])*gwT[k];
+								gweights += gwT[k];
+							}
+							s1 += pitch;
+							s2 += pitch;
+							gwT += Sxd;
+						}
+						const int weight = exp((diff/gweights)*hin) * PRECISION;
+						const int xRb = MIN(MIN(this.Bx,widthm1-v),widthm1-x);
+						const unsigned char *sbp = sbp_saved + v;
+						int *sumsbT = sumsb_saved;
+						int *weightsbT = weightsb_saved;
+						// int j;
+						for ( j=-this.By; j<=yBb; ++j)
+						{
+							int k;
+							for ( k=-this.Bx; k<=xRb; ++k)
+							{
+								sumsbT[k] += sbp[k]*weight;
+								weightsbT[k] += weight;
+							}
+							sumsbT += Bxd;
+							weightsbT += Bxd;
+							sbp += pitch;
+						}
+						if (weight > wmax) wmax = weight;
+					}
+				}
+				const unsigned char *srcpT = srcp+x-this.Bx;
+				unsigned char *dstpT = dstp+x-this.Bx;
+				int *sumsbTr = this.sumsb;
+				int *weightsbTr = this.weightsb;
+				if (wmax <= 1) wmax = PRECISION;
+				int j;
+				for ( j=0; j<yTr; ++j)
+				{
+					int k;
+					for ( k=0; k<xTr; ++k)
+					{
+						sumsbTr[k] += srcpT[k]*wmax;
+						weightsbTr[k] += wmax;
+						dstpT[k] = MAX(MIN((int)((sumsbTr[k]/weightsbTr[k])+0.5),255),0);
+					}
+					srcpT += pitch;
+					dstpT += pitch;
+					sumsbTr += Bxd;
+					weightsbTr += Bxd;
+				}
+			}
+			dstp += pitch*Byd;
+			srcp += pitch*Byd;
+		}
+	}
+}
+
+
+
+
+static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si) {	
+	int b;
+	
+	int Sxd = this.Sx*2+1;
+	int Syd = this.Sy*2+1;
+	
+	double 	h2in = -1.0/(this.h*this.h);
+	double hin = -1.0/this.h;
 	
 	
 	//fprintf (stderr,"trace: filterframe in\n");
@@ -198,16 +331,16 @@ static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, do
 		const int widthm1 = width-1;
 		const int pitch = width ;
 
-		memset(dssum,0,height * width * sizeof(double));
-		memset(dsweight,0,height * width * sizeof(double));
-		memset(dswmax,0,height * width * sizeof(double));
+		memset(this.dsum,0,height * width * sizeof(int));
+		memset(this.dweight,0,height * width * sizeof(int));
+		memset(this.dwmax,0,height * width * sizeof(int));
 
 		int y;
 	//	fprintf (stderr,"trace: filterframe y\n");
 
 		for (y=0; y < height; ++y) {
 			
-			const int stopy = MIN(y+Ay,heightm1);
+			const int stopy = MIN(y+this.Ay,heightm1);
 			const int doffy = y*width;
 			int x;
 			
@@ -215,16 +348,16 @@ static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, do
 
 			for (x=0; x < width; ++x) {
 				
-				const int startxt = MAX(x-Ax,0);
-				const int stopx = MIN(x+Ax,widthm1);
+				const int startxt = MAX(x-this.Ax,0);
+				const int stopx = MIN(x+this.Ax,widthm1);
 				const int doff = doffy+x;
 				
 			//	fprintf (stderr,"trace: filterframe doff=%d\n",doff);
 
 				
-				double *dsum = &dssum[doff];
-				double *dweight = &dsweight[doff];
-				double *dwmax = &dswmax[doff];
+				int *dsum = &this.dsum[doff];
+				int *dweight = &this.dweight[doff];
+				int *dwmax = &this.dwmax[doff];
 				
 				int u;
 // fprintf (stderr,"trace: filterframe u in\n");
@@ -232,11 +365,11 @@ static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, do
 				for (u=y; u<=stopy; ++u)
 				{
 					const int startx = u == y ? x+1 : startxt;
-					const int yT = -MIN(MIN(Sy,u),y);
-					const int yB = MIN(MIN(Sy,heightm1-u),heightm1-y);
+					const int yT = -MIN(MIN(this.Sy,u),y);
+					const int yB = MIN(MIN(this.Sy,heightm1-u),heightm1-y);
 					const unsigned char *s1_saved = pfp + (u+yT)*pitch;
 					const unsigned char *s2_saved = pfp + (y+yT)*pitch + x;
-					const double *gw_saved = gw+(yT+Sy)*Sxd+Sx;
+					const int *gw_saved = this.gw+(yT+this.Sy)*Sxd+this.Sx;
 					const int pfpl = u*pitch;
 					const int coffy = u*width;
 					
@@ -246,15 +379,15 @@ static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, do
 					for ( v=startx; v<=stopx; ++v)
 					{
 						const int coff = coffy+v;
-						double *csum = &dssum[coff];
-						double *cweight = &dsweight[coff];
-						double *cwmax = &dswmax[coff];
-						const int xL = -MIN(MIN(Sx,v),x);
-						const int xR = MIN(MIN(Sx,widthm1-v),widthm1-x);
+						int *csum = &this.dsum[coff];
+						int *cweight = &this.dweight[coff];
+						int *cwmax = &this.dwmax[coff];
+						const int xL = -MIN(MIN(this.Sx,v),x);
+						const int xR = MIN(MIN(this.Sx,widthm1-v),widthm1-x);
 						const unsigned char *s1 = s1_saved + v;
 						const unsigned char *s2 = s2_saved;
-						const double *gwT = gw_saved;
-						double diff = 0.0, gweights = 0.0;
+						const int *gwT = gw_saved;
+						int diff = 0, gweights = 0;
 						
 						int j;
 				//		fprintf (stderr,"trace: filterframe j in\n");
@@ -276,7 +409,15 @@ static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, do
 							s2 += pitch;
 							gwT += Sxd;
 						}
-						const double weight = exp((diff/gweights)*hin);
+						int weight;
+						if (diff != 0) {
+							//fprintf(stderr,"exp of  %f\n",exp(diff/gweights)*hin);
+							
+							// what is this doing in the middle of processing
+							weight = exp((diff/gweights)*hin) * PRECISION;
+						} else {
+							weight = 1;
+						}
 						*cweight += weight;
 						*dweight += weight;
 						*csum += srcp[x]*weight;
@@ -285,7 +426,7 @@ static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, do
 						if (weight > *dwmax) *dwmax = weight;
 					}
 				}
-					const double wmax = *dwmax <= DBL_EPSILON ? 1.0 : *dwmax;
+					const int wmax = *dwmax <= 1 ? PRECISION : *dwmax;
 					*dsum += srcp[x]*wmax;
 					*dweight += wmax;
 					dstp[x] = MAX(MIN((int)(((*dsum)/(*dweight))+0.5),255),0);
@@ -321,83 +462,6 @@ static void filter(int fdIn, int fdOut, y4m_stream_info_t  *inStrInfo )
 	/* Initialize counters */
 	
 	
-	int Ax,Ay,Bx,By,Sx,Sy,a,h;
-	
-	// Window size
-	// trial values, will move to Command line arguments
-	a = 2;
-	h = 1;
-	
-	Ax = 4; 
-	Ay = 4;
-	
-	Sy = 4;
-	Sx = 4;
-	
-	Bx = 4;
-	By = 4;
-	
-	int Sxd = Sx*2+1;
-	int Syd = Sy*2+1;
-	
-	int Sxa = Sxd*Syd;
-	int Bxd = Bx*2+1;
-	int Byd = By*2+1;
-	int Bxa = Bxd*Byd;
-	int Axd = Ax*2+1;
-	int Ayd = Ay*2+1;
-	int Axa = Axd*Ayd;
-	// Azdm1 = Az*2;
-	int a2 = a*a;
-	
-	
-	const int Sxh = (Sx+1)>>1;
-	const int Syh = (Sy+1)>>1;
-	const int Sxdh = Sxh*2+1;
-	const int Sydh = Syh*2+1;
-	const int Bxh = (Bx+1)>>1;
-	const int Byh = (By+1)>>1;
-	
-	
-	
-	double *gwh = (double*)malloc(Sxdh*Sydh*sizeof(double));
-	int w = 0, m, n;
-	int j;
-	for ( j=-Syh; j<=Syh; ++j)
-	{
-		if (j < 0) m = MIN(j+Byh,0);
-		else m = MAX(j-Byh,0);
-		int k;
-		for ( k=-Sxh; k<=Sxh; ++k)
-		{
-			if (k < 0) n = MIN(k+Bxh,0);
-			else n = MAX(k-Bxh,0);
-			gwh[w++] = exp(-((m*m+n*n)/(2*a2)));
-		}
-	}
-	
-	int framesize = y4m_si_get_plane_length(inStrInfo,0);
-	
-	double *dsum = (double *)malloc(framesize*sizeof(double));
-	double *dweight = (double *)malloc(framesize*sizeof(double));
-	double *dwmax = (double *)malloc(framesize*sizeof(double));
-	
-	double *gw = (double*)malloc(Sxd*Syd*sizeof(double));
-	
-//	int w = 0, m, n;
-	// int j;
-	for ( j=-Sy; j<=Sy; ++j)
-	{
-		if (j < 0) m = MIN(j+By,0);
-		else m = MAX(j-By,0);
-		int k;
-		for ( k=-Sx; k<=Sx; ++k)
-		{
-			if (k < 0) n = MIN(k+Bx,0);
-			else n = MAX(k-Bx,0);
-			gw[w++] = exp(-((m*m+n*n)/(2*a2)));
-		}
-	}
 	
 	
 	write_error_code = Y4M_OK ;
@@ -412,8 +476,11 @@ static void filter(int fdIn, int fdOut, y4m_stream_info_t  *inStrInfo )
 			
 			// static void filterframe (uint8_t *m[3], uint8_t *n[3], y4m_stream_info_t *si, double *dsum, double *dweight, double *dwmax, int Ax, int Ay, int Sx, int Sy, double *gw)
 
-			
-			filterframe(yuv_odata,yuv_data,inStrInfo,dsum,dweight,dwmax,Ax,Ay,Sx,Sy,h,gw);
+			if (this.Bx || this.By) {
+				filterframeB(yuv_odata,yuv_data,inStrInfo);
+			} else {
+				filterframe(yuv_odata,yuv_data,inStrInfo);
+			}
 			write_error_code = y4m_write_frame( fdOut, inStrInfo, &in_frame, yuv_odata );
 		}
 		
@@ -447,13 +514,10 @@ int main (int argc, char *argv[])
 	int interlaced,ilace=0,pro_chroma=0,yuv_interlacing= Y4M_UNKNOWN;
 	int height;
 	int c ;
-	const static char *legal_flags = "v:hr:d:i";
+	const static char *legal_flags = "v:hr:d:";
 	
 	float sigma;
 	
-	this.sigmaR = 0;
-	this.sigmaD = 0;
-	this.direction = 0;
 	
 	while ((c = getopt (argc, argv, legal_flags)) != -1) {
 		switch (c) {
@@ -470,14 +534,9 @@ int main (int argc, char *argv[])
 				break;
 			case 'r':
 				sigma = atof(optarg);
-				this.sigmaR = sigma * PRECISION;
 				break;
 			case 'd':
 				sigma = atof(optarg);
-				this.sigmaD = sigma * PRECISION;
-				break;
-			case 'i':
-				this.direction = 1;
 				break;
 				
 		}
@@ -499,17 +558,17 @@ int main (int argc, char *argv[])
 		mjpeg_error_exit1 ("Could'nt read YUV4MPEG header!");
 	
 	// Information output
-	mjpeg_info ("yuvbilateral (version " VERSION ") is a spatial bilateral filter for yuv streams");
+	mjpeg_info ("yuvnlmeans (version " VERSION ") is a spatial bilateral filter for yuv streams");
 	mjpeg_info ("(C) 2010 Mark Heath <mjpeg0 at silicontrip.org>");
 	mjpeg_info ("yuvbilateral -h for help");
 	
     
 	/* in that function we do all the important work */
-//	filterinitialize ();
+	filterinitialize (4,4,2,2,2,2,1.0,1.0,&in_streaminfo);
 	y4m_write_stream_header(fdOut,&in_streaminfo);
 	filter(fdIn,fdOut, &in_streaminfo);
 	y4m_fini_stream_info (&in_streaminfo);
-	//filteruninitialize();
+	filteruninitialize();
 	
 	return 0;
 }
