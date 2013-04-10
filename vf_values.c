36d7246971c841de552230a2474c3485d292128d
Mark Heath (@utopia) <mjpeg0@silicontrip.net>
2013-04-10 10:27:53 +1000

    vf_values now builds.

--- vf_values.c
+++ vf_values.c
@@ -31,69 +31,61 @@
 #include "internal.h"
 #include "video.h"
 
+#include "libavutil/opt.h"
+
 
 #include <strings.h>
 #include <fcntl.h>
 
 /* time
- 10 April 06:58 - 07:
+ 10 April 06:58 - 07:38
+ 
  */
 
 typedef struct
 {	
-	int fd;
+	FILE  *fh;
     char *filename;
-    
     
     int chromah;
     int chromaw;
     
 } valuesContext;
 
+#define OFFSET(x) offsetof(valuesContext, x)
+
 static const AVOption values_options[]= {
-    {"filename", "set output file", OFFSET(filename), AV_OPT_TYPE_STRING, {.str=NULL},  CHAR_MIN, CHAR_MAX, FLAGS},
-    {"f", "set output file", OFFSET(filename), AV_OPT_TYPE_STRING, {.str=NULL},  CHAR_MIN, CHAR_MAX, FLAGS},
-    {NULL},
+    {"filename", "set output file", OFFSET(filename), AV_OPT_TYPE_STRING, {.str=NULL},  CHAR_MIN, CHAR_MAX},
+    {"f", "set output file", OFFSET(filename), AV_OPT_TYPE_STRING, {.str=NULL},  CHAR_MIN, CHAR_MAX},
+    {NULL}
 };
 
-/*
- static av_cold void uninit(AVFilterContext *ctx)
- {
- levelsContext *ovl = ctx->priv;
- if (ovl->pFrame)
- av_free(ovl->pFrame);
- 
- if (ovl->maskFrame) 
- av_free(ovl->maskFrame);
- 
- if (ovl->imageName)
- av_free(ovl->imageName);
- 
- ovl->imageName = NULL;
- ovl->pFrame = NULL;
- ovl->maskFrame = NULL;
- }
- */
 
 // static av_cold int init(AVFilterContext *ctx, const char *args, void *opaque)
 static av_cold int init(AVFilterContext *ctx, const char *args)
 {
     valuesContext *valuesCtx = ctx->priv;
-	int argc=0;
-	
+    int ret;
+
     
     av_opt_set_defaults(valuesCtx);
 
     if ((ret = av_set_options_string(valuesCtx, args, "=", ":")) < 0)
         return ret;
 
-    valuesCtx->fd = 1;
+    valuesCtx->fh = stdout;
     
     if (valuesCtx->filename != NULL)
     {
-        valuesCtx->fd = open (valuesCtx->filename,O_WRONLY|O_CREAT,0644);
+        valuesCtx->fh = fopen (valuesCtx->filename,"w");
     }
 	return 0;
+}
+
+static av_cold void uninit(AVFilterContext *ctx)
+{
+    valuesContext *valuesCtx = ctx->priv;
+    fclose(valuesCtx->fh);
 }
 
 static int query_formats(AVFilterContext *ctx)
@@ -140,24 +132,56 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
     valuesContext *valuesCtx = link->dst->priv;
     AVFilterLink *outlink = link->dst->outputs[0];
     AVFrame *out; // = link->dst->outputs[0]->outpic;
-	AVFilterContext *ctx = link->src;
-
-
-    int h = outlink->h;
     
-	// Broadcast swing levels, min and max
-	int min=16;
-	int max=235;
-	int luma,chromab,chromar;
 	int i,j;
-	
+	int cw,w;
+    int yuv;
+    
+    
+	int miny,minu,minv;
+	int toty=0,totu=0,totv=0;
+	int maxy,maxu,maxv;
+
+    
     out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
     av_frame_copy_props(out, in);
 
-		
+    miny = in->data[0][0];
+	maxy = in->data[0][0];
+	
+	minu = in->data[1][0];
+	maxu = in->data[1][0];
+	
+	minv = in->data[2][0];
+	maxv = in->data[2][0];
+
+    cw = 0; w = 0;
     for (j=0; j<link->h; j++) {
         for (i=0;i<link->w;i++) {
+           
+            yuv = in->data[0][w+i];
+
+            if (yuv > maxy) maxy=yuv;
+			if (yuv < miny) miny=yuv;
+			toty += yuv;
+
+            if (i<valuesCtx->chromaw && j<valuesCtx->chromah) {
+				yuv = in->data[1][cw+i];
+				if (yuv > maxu) maxu=yuv;
+				if (yuv < minu) minu=yuv;
+				totu += yuv;
+				
+				yuv = in->data[2][cw+i];
+				if (yuv > maxv) maxv=yuv;
+				if (yuv < minv) minv=yuv;
+				totv += yuv;
+			}
+
+            
         }
+        
+        w += in->linesize[0];
+        cw += in->linesize[1];
 	}
 	
     // should I copy the frame to the output?
@@ -166,7 +190,7 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
     return ff_filter_frame(outlink, out);
 }
 
-static const AVFilterPad avfilter_vf_broadcast_inputs[] = {
+static const AVFilterPad avfilter_vf_values_inputs[] = {
     {
         .name            = "default",
         .type            = AVMEDIA_TYPE_VIDEO,
@@ -177,7 +201,7 @@ static const AVFilterPad avfilter_vf_broadcast_inputs[] = {
     { NULL }
 };
 
-static const AVFilterPad avfilter_vf_broadcast_outputs[] = {
+static const AVFilterPad avfilter_vf_values_outputs[] = {
     {
         .name            = "default",
         .config_props     = config_props,
@@ -187,16 +211,16 @@ static const AVFilterPad avfilter_vf_broadcast_outputs[] = {
 };
 
 
-AVFilter avfilter_vf_broadcast = {
-    .name      = "broadcast",
-	.description = "clips or scales full swing levels to broadcast swing.",
+AVFilter avfilter_vf_values = {
+    .name      = "values",
+	.description = ".",
 	
 	.init = init,
-	//    .uninit    = uninit,
+	.uninit    = uninit,
     .query_formats = query_formats,
 	
-	.priv_size = sizeof(levelsContext),
+	.priv_size = sizeof(valuesContext),
 	
-    .inputs    = avfilter_vf_broadcast_inputs,
-    .outputs   = avfilter_vf_broadcast_outputs,
+    .inputs    = avfilter_vf_values_inputs,
+    .outputs   = avfilter_vf_values_outputs,
 };
