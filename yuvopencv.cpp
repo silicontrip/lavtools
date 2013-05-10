da2b48b40ec3b6b28690ff306e11b2d40b06ca83
Mark Heath (@utopia) <mjpeg0@silicontrip.net>
2013-05-11 00:31:07 +1000

    motion detection by phase correlation.

--- yuvopencv.cpp
+++ yuvopencv.cpp
@@ -48,7 +48,7 @@ int main (int argc, char **argv)
 
         oyuv.dumpInfo();
         
-        oyuv.writeHeader();
+    //    oyuv.writeHeader();
     
         iyuv.allocFrameData();
         oyuv.allocFrameData();
@@ -67,34 +67,52 @@ int main (int argc, char **argv)
         memset (oyuv.getYUVFrame()[1],128,cwidth*cheight);
         memset (oyuv.getYUVFrame()[2],128,cwidth*cheight);
 
-        img1 = cv::Mat(height,width,CV_8UC1,oyuv.getYUVFrame()[0],width);
-        imgout.create(height,width,CV_8UC1);
+     //   img1 = cv::Mat(height,width,CV_8UC1,oyuv.getYUVFrame()[0],width);
+        img1.create(height,width,CV_32FC1);
         //imgout = img1.clone();
         
         cv::Point2d shift;
         
+        cv::Point2d acc (0,0);
+
         while(1) {
             iyuv.read();
         
-            img2 = cv::Mat(height,width,CV_8UC1,iyuv.getYUVFrame()[0],width);
-            //imgout = img2.clone();
+            imgout = cv::Mat(height,width,CV_8UC1,iyuv.getYUVFrame()[0],width);
+            
+            imgout.assignTo(img2,CV_32FC1);
+            
+        //    imgout = img2.clone();
   
-            cv::accumulate(img2, imgout);
+        //    cv::accumulate(img2, imgout);
             
+       //     std::cerr << "img1: " << img1.type() << " / img2: " << img2.type() << "\n";
         
-//            shift = cv::phaseCorrelate(img1,img2);
+            shift = cv::phaseCorrelate(img1,img2);
       
-  //          std::cout << shift << "\n";
+            acc = acc + shift;
             
-    //        img1 = img2.clone();
+       //     std::cout << shift << " - " << acc  << "\n";
+         
+            std::cout << acc << "\n";   
             
-
-            int y;
-            for (y=0;y<height;y++)
+            img1 = img2.clone();
+            
+  /*
+            if (imgout.step == width)
             {
-                memcpy(oyuv.getYUVFrame()[0] + y * width,imgout.ptr(y),width);
+                oyuv.getYUVFrame()[0] = imgout.ptr(0);
+            }
+            else
+            {
+                int y;
+                for (y=0;y<height;y++)
+                {
+                    memcpy(oyuv.getYUVFrame()[0] + y * width,imgout.ptr(y),width);
+                }
             }
             oyuv.write();
+   */
         }
     } catch (AVException *e) {
 		std::cerr << "ERROR occurred: " << e->getMessage() << "\n";
